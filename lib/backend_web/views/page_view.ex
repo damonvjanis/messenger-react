fa818e89d93b3235d130879d6949eb53aaa7f9b3
Damon Janis <damonvjanis@gmail.com>
2020-04-10 06:59:45 -0600

    Add logs

--- lib/backend_web/views/page_view.ex
+++ lib/backend_web/views/page_view.ex
@@ -2,8 +2,11 @@ defmodule BackendWeb.PageView do
   use BackendWeb, :view
 
   def render("index.html", _assigns) do
-    Path.join(:code.priv_dir(:backend), "static/build/index.html")
+    Path.join(:code.priv_dir(:backend) |> IO.inspect(), "static/build/index.html")
+    |> IO.inspect()
     |> File.read!()
+    |> IO.inspect()
     |> raw()
+    |> IO.inspect()
   end
 end
