f4aaaf1d805f1f00d30cc8841d8441e5c4dab6b6
Damon Janis <damonvjanis@gmail.com>
2020-04-12 08:26:19 -0600

    Consolidate migrations

--- priv/repo/migrations/20191109225222_create_conversations.exs
+++ priv/repo/migrations/20191109225222_create_conversations.exs
@@ -5,10 +5,12 @@ defmodule Backend.Repo.Migrations.CreateConversations do
     create table(:conversations) do
       add :name, :string
       add :number, :string
+      add :unread_at, :naive_datetime_usec
 
       timestamps()
     end
 
+    create index(:conversations, [:unread_at])
     create unique_index(:conversations, [:number])
   end
 end
